67bb1b3cb1b1a29ea058fd5ca42611a5af2c4006
Benard Mesander <ben@Benards-MacBook-Pro.local>
2025-06-20 07:10:49 -0600

    factor constants out into a header file

---
+++ src/float.s
@@ -0,0 +1,20 @@
+### Single precision
+
+.equ SP_MANTISSA_BITS, 23
+.equ SP_EXPONENT_BITS, 8
+.equ SP_EXPONENT_BIAS, 127
+
+.equ SP_ZERO, 0x00000000
+.equ SP_MINUS_ZERO, 0x80000000
+.equ SP_QNAN, 0x7FC00000
+.equ SP_SNAN, 0x7F800001
+.equ SP_INF, 0x7F800000
+.equ SP_MINUS_INF, 0xFF800000
+
+### Double precision
+
+### Half precision
+	
+### Bfloat16
+
+	
